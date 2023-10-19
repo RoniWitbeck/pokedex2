d4b6cb6a3733c37dd6f68a175118c0f34deb6521
_Roni W_ <_[roni.witbeck@gmail.com](mailto:roni.witbeck@gmail.com)_>
2023-10-19 11:38:32 -0700

    more progress

---
+++ pokedex_project/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Homepage from './pages/Homepage';
+import PokemonPage from './pages/PokemonPage';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+        <Container>
+          <Routes>
+            <Route exact path='/' element={ <Homepage/> }/>
+            <Route path='/pokemon/:id' element={ <PokemonPage/> }/>
+          </Routes>
+        </Container>
+    </Router>
+  );
+}
+
+export default App;
